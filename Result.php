62c4f7f197fb56a2d25b43b15ee5dddfabbf9414
Edwin <edwinendingii@example.com>
2019-02-06 11:17:31 +0800

    Simple Calculator

---
+++ Result.php
@@ -0,0 +1,17 @@
+<!DOCTYPE>
+<html>
+<title>Group2</title>
+<body>
+	
+	<?php
+	 
+	 $number1 = $_POST['number1'];
+	 $number2 = $_POST['number2'];
+	 $sum = $number1 + $number2;
+
+	 ?>
+		
+	<?php echo "The sum of ".$number1. " & " .$number2. " is "  . $sum;?>
+
+</body>
+</html>
